31c9eaa75e29f9c87b1e9aae3a256a49d7b0f434
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2018-12-15 20:52:46 +0100

    Added URL based XML resolution methods.

--- core/src/main/java/es/upm/tfo/lst/CodeGenerator/xmlparser/XmlParser.java
+++ core/src/main/java/es/upm/tfo/lst/CodeGenerator/xmlparser/XmlParser.java
@@ -1,12 +1,9 @@
 package es.upm.tfo.lst.CodeGenerator.xmlparser;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -37,14 +34,12 @@ import es.upm.tfo.lst.CodeGenerator.model.Variable;
 public class XmlParser {
 
 	private final static Logger log = Logger.getLogger(GenerateProject.class);
-	private File xmlFile;
-	private String xmlPath;
+	private URL xmlPath;
 	private NodeList nodeVariable, nodeMacro,templateName, templateVersion,templateDescription,templateAuthor;
 	private Map<String,Variable> variableList;
 	private List<MacroModel> macroList;
 	private TemplateDataModel javaXMLModel = null;
 	private Author author;
-	private URL url=null;
 
 	public XmlParser() {
 			this.variableList = new HashMap<>();
@@ -57,17 +52,29 @@ public class XmlParser {
 	 * @param xmlPath {@link String } path to XML file
 	 */
 	public void generateXMLCoordinator(String xmlPath){
-		this.xmlPath = xmlPath;
 		try {
-			this.url = new URL(this.xmlPath);
+			this.xmlPath =  new URL(xmlPath);;
 			//readFromURL(url, "");
 		}catch (Exception e) {
-			log.warn("given URL ist valid, trying to load from filesystem");
-			this.readFromLocalFileSystem();
+			log.warn("given URL ist valid, trying to interpret as filesystem");
+			try {
+				this.xmlPath = new File(xmlPath).toURI().toURL();;
+			} catch (Exception e2) {
+				log.fatal("giving up.", e2);
+			}
 		}
-		//this.readXML(xmlPath);
+		this.readFromLocalFileSystem();
 	}
 
+	/**
+	 *generate XmlCoordinator object who represent XML file into java code
+	 * @param xmlPath {@link URL } path to XML file
+	 */
+	public void generateXMLCoordinator(URL xmlPath){
+		this.xmlPath = xmlPath;
+		this.readFromLocalFileSystem();
+	}
+	
 	/**
 	 *
 	 * @return {@link TemplateDataModel} object. Null if method {@link #generateXMLCoordinator(String)} isn't called
@@ -88,10 +95,9 @@ public class XmlParser {
 		try {
 
 			this.javaXMLModel = new TemplateDataModel();
-			this.xmlFile=new File(this.xmlPath);
 			 DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
 	         DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
-	         Document doc = docBuilder.parse (xmlFile);
+	         Document doc = docBuilder.parse (xmlPath.openStream());
 	         this.nodeMacro = doc.getElementsByTagName("macro");
 	         this.nodeVariable = doc.getElementsByTagName("variable");
 	         this.templateAuthor = doc.getElementsByTagName("template-author");
@@ -146,45 +152,10 @@ public class XmlParser {
 			this.javaXMLModel = null;
 		}
 
-
-
 	}
-	
-	/**
-	 * method to get XML file readed of website
-	 * @param url
-	 * @throws Exception
-	 */
-	  private void readFromURL(URL url,String targetDir) throws Exception{
-		  	 StringBuilder sb = new StringBuilder();
-		     //URI uri = new URI("http://localhost/templates/simple.xml");
-	         BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-	         String inputLine;
-	         while ((inputLine = in.readLine()) != null)
-	        	 sb.append(inputLine+"\n");
-	             System.out.println(inputLine);
-	         in.close();
-			BufferedWriter bw = null;
-			FileWriter fw = null;
-            fw = new FileWriter(targetDir);
-			bw = new BufferedWriter(fw);
-			bw.write(sb.toString());
-			bw.close();
 
-	    }
 
-	  /**
-	   * Method who return the parent URL or File of selected XML.
-	   * @return
-	   */
-	 public String getParentTemplateDir() throws Exception{
-		 URI uri;
-		 if(this.url==null)
-			 return new File(this.xmlPath).getParentFile().getPath();
-		 else {
-			 uri = this.url.toURI().resolve(".");
-			 return uri.getPath();
-		 }
-			 
-	 } 
+	 public URL getParentTemplateDir() throws MalformedURLException, URISyntaxException {
+		 return xmlPath.toURI().resolve("..").toURL();
+	 }
 }
