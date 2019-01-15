5142d844bce1f724c0a7a90a2bc8d3d0f9040539
eduardo <ebuhid@lst.tfo.upm.es>
2019-01-15 12:33:32 +0100

    added custom exceptions to project.

---
+++ codegenerator.core/src/main/java/es/upm/tfo/lst/CodeGenerator/exception/XmlCoordinatorException.java
@@ -0,0 +1,30 @@
+package es.upm.tfo.lst.CodeGenerator.exception;
+
+public class XmlCoordinatorException extends Exception {
+
+	
+	public XmlCoordinatorException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public XmlCoordinatorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public XmlCoordinatorException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public XmlCoordinatorException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public XmlCoordinatorException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+}
