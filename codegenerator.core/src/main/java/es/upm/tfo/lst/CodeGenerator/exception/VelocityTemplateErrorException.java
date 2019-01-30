3ca4342e9cc63e852e21404d3c88de4779cc3803
eduardo <ebuhid@lst.tfo.upm.es>
2019-01-30 16:47:33 +0100

    class to handle velocity template errors

---
+++ codegenerator.core/src/main/java/es/upm/tfo/lst/CodeGenerator/exception/VelocityTemplateErrorException.java
@@ -0,0 +1,29 @@
+package es.upm.tfo.lst.CodeGenerator.exception;
+
+public class VelocityTemplateErrorException extends Exception{
+	
+	public VelocityTemplateErrorException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public VelocityTemplateErrorException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VelocityTemplateErrorException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VelocityTemplateErrorException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VelocityTemplateErrorException(String message, Throwable cause, boolean enableSuppression,boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
