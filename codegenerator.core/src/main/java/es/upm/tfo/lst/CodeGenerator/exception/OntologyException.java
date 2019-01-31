74c59f6c6c9b30575a77e1edeacc87772fc77e03
eduardo <ebuhid@lst.tfo.upm.es>
2019-01-31 10:00:03 +0100

    added new exception

---
+++ codegenerator.core/src/main/java/es/upm/tfo/lst/CodeGenerator/exception/OntologyException.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package es.upm.tfo.lst.CodeGenerator.exception;
+
+/**
+ * @author Eduardo
+ *
+ */
+public class OntologyException extends Exception {
+
+	/**
+	 * 
+	 */
+	public OntologyException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public OntologyException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public OntologyException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public OntologyException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public OntologyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
